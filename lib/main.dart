637bf34a5f5c144c926ca147663b2d58a2d8d8be
kuanxdd <gesarlee.kuanfu@gmail.com>
2023-01-11 00:14:57 +0800

    Remove API Key

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:my_rescue/config/themes/theme_config.dart';
 
-import 'modules/screens/homepage.dart';
+import 'package:my_rescue/modules/screens/homepage.dart';
+//import 'package:my_rescue/modules/screens/homepage.dart';
+//import 'package:my_rescue/modules/screens/victim-help-page.dart';
+//import 'package:my_rescue/modules/screens/safety-guidelines.dart';
+//import 'package:my_rescue/modules/screens/help-map.dart';
+//import 'package:my_rescue/modules/screens/help-submmited-page.dart';
 
 void main() {
   runApp(const MyApp());

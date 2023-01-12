ee08e7ebeb770d9912d70370ea8281f29137418d
Ritchie <ritchiepoh@gmail.com>
2023-01-12 16:39:16 +0800

    deleted redundant env folder

--- lib/env/env.dart
+++
@@ -1,9 +0,0 @@
-import 'package:envied/envied.dart';
-
-part 'env.g.dart';
-
-@Envied(path: '.env')
-abstract class Env {
-  @EnviedField(varName: 'GOOGLE_MAP_API_KEY')
-  static const googleMapApiKey = _Env.googleMapApiKey;
-}

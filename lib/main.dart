4532591bb8ca2821f7077b199522f31d09f859db
Ritchie <ritchiepoh@gmail.com>
2023-01-16 15:14:18 +0800

    Update gitignore and render Homepage at start

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: appThemeData,
-      home: LoginPage(),
+      home: const HomePage(),
       // Disable the debug flag
       debugShowCheckedModeBanner: false,
     );
